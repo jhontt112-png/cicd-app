e7a0dec48542e176bd0e36e54b6619015ac0c963
jhon tt <jhontt112y@gmail.com>
2026-03-02 22:52:47 +0530

    initial commit

---
+++ src/main/java/com/devops/cicd_app/CicdAppApplication.java
@@ -0,0 +1,12 @@
+package com.devops.cicdapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CicdAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CicdAppApplication.class, args);
+    }
+}

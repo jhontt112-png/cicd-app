875105028adbab90a9299ea402845fee5860fea1
jhon tt <jhontt112y@gmail.com>
2026-03-03 17:06:18 +0530

    Fixed SpringBoot test configuration

--- src/test/java/com/devops/cicd_app/CicdAppApplicationTests.java
+++ src/test/java/com/devops/cicd_app/CicdAppApplicationTests.java
@@ -3,7 +3,7 @@ package com.devops.cicd_app;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = CicdAppApplication.class)
 class CicdAppApplicationTests {
 
     @Test
